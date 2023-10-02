9762b94c7afa8ed52f2ccbd193ef477143e63611
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 01:34:25 +0200

    fix table

--- src/components/TransactionListContainer.tsx
+++ src/components/TransactionListContainer.tsx
@@ -40,25 +40,33 @@ export default function TransactionListContainer({
         <tbody className="">
           {data.length >= 1 ? (
             data.map((transaction) => (
-              <tr
-                key={transaction.transaction.id}
-                className="  cursor-pointer "
-              >
+              <tr key={transaction.transaction.id} className=" cursor-pointer ">
                 <th
                   scope="row"
-                  className="px-6 py-4 font-medium  whitespace-nowrap  capitalize rounded-l-xl border-2 border-r-0 border-gray-200"
+                  className="px-6 py-4 font-medium  whitespace-nowrap  capitalize rounded-l-xl border-2 border-r-0 border-gray-200 h-20"
                 >
-                  <p>{transaction.transaction.name}</p>
-                  <p>{transaction.transaction.id}</p>
+                  <p className="text-lg">{transaction.transaction.name}</p>
+                  <p className="text-base font-light">
+                    {transaction.transaction.id}
+                  </p>
                 </th>
-                <td className="px-6 py-4 capitalize border-t-2 text-lg border-b-2 border-gray-200">
+                <td className="px-6 py-4 capitalize border-t-2 text-lg border-b-2 border-gray-200  h-20">
                   {transaction.amount} NOK
                 </td>
-                <td className="px-6 py-4 border-t-2 border-b-2 text-lg border-gray-200 md:block hidden">
+                <td className="px-6 py-4 border-t-2 border-b-2 text-lg border-gray-200 md:flex hidden h-24  flex-row items-center">
                   {transaction.date}
                 </td>
-                <td className="px-6 py-4 border-l-0 border-2 text-lg border-gray-200 rounded-r-xl">
-                  <div className="bg-red-600 text-red-800 font-medium text-lg bg-opacity-20 text-center rounded-lg h-9 px-3 flex justify-center items-center">
+                <td className="px-6 py-4 border-l-0 border-2 text-lg border-gray-200 rounded-r-xl h-20 ">
+                  <div
+                    className={`${
+                      (transaction.status == "Pending" &&
+                        "bg-red-600 text-red-800") ||
+                      (transaction.status == "Done" &&
+                        "bg-green-600 text-green-800") ||
+                      (transaction.status == "Ongoing" &&
+                        "bg-yellow-400 text-yellow-600")
+                    } font-medium text-lg bg-opacity-20 text-center rounded-lg h-9 px-3 flex justify-center items-center`}
+                  >
                     {transaction.status}
                   </div>
                 </td>
--- src/mock-data/data.ts
+++ src/mock-data/data.ts
@@ -1,1 +1,31 @@
-export const transactionData = [];
+import { TransactionData } from "@/interfaces/interfaces";
+
+export const transactionData: TransactionData[] = [
+  {
+    transaction: {
+      name: "Sophie",
+      id: "234-342-222",
+    },
+    amount: 1000,
+    status: "Ongoing",
+    date: "20. Sep 2023",
+  },
+  {
+    transaction: {
+      name: "Sophie",
+      id: "234-342-223",
+    },
+    amount: 1000,
+    status: "Done",
+    date: "20. Sep 2023",
+  },
+  {
+    transaction: {
+      name: "Sophie",
+      id: "234-342-224",
+    },
+    amount: 1000,
+    status: "Pending",
+    date: "20. Sep 2023",
+  },
+];
