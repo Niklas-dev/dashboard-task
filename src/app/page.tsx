a25453c8c20cfb8d7e20470d50bcb7088d372a1a
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-02 18:20:42 +0200

    add landing page info container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-screen w-screen">
-      <div className="w-96 h-96 bg-gray-100 rounded-xl"></div>
+    <main className="flex justify-center items-center h-screen w-screen bg-gray-100">
+      <div className="w-96 h-80  shadow-md rounded-xl p-4 bg-white ring-primaryRed ring-2 flex flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl text-center text-primaryRed font-medium">
+            Welcome to my task
+          </h1>
+          <p className="text-center text-textGray mt-6">
+            When you click the button below you will get to the dashboard page.
+          </p>
+          <Link
+            className="bg-primaryRed rounded-lg text-white px-3 py-2.5 mt-4"
+            href={"/dashboard"}
+          >
+            To Dashboard
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
