c971a262e2089cc7585b202f643327b0059d27f0
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-02 18:11:39 +0200

    add colors from evvent.no

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="flex justify-center items-center"></main>;
+  return (
+    <main className="flex justify-center items-center h-screen w-screen">
+      <div className="w-96 h-96 bg-gray-100 rounded-xl"></div>
+    </main>
+  );
 }
