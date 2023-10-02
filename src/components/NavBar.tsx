d49c707f5515d7f2760d3be712d431bbd2b24c9c
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-02 19:48:39 +0200

    fix navbar shadow

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import UserInfoElements from "./UserInfoElements";
 
 export default function NavBar() {
   return (
-    <nav className="flex flex-row justify-end lg:justify-between py-4 px-6 border-b border-gray-200 drop-shadow-sm relative z-10">
+    <nav className="flex flex-row justify-end lg:justify-between py-4 px-6 border-b border-gray-200 drop-shadow-md relative z-10">
       <NavElements />
       <UserInfoElements />
     </nav>
