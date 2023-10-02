f303e03d7f7962de019f9af2958bc756340efbc5
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-02 18:40:09 +0200

    setup layout for NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,26 @@
+import Link from "next/link";
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav className="flex flex-row justify-between py-4 px-6 border-b-2 border-gray-200 drop-shadow-sm">
+      <div className="flex flex-row items-center gap-14">
+        <Link
+          className="bg-primaryRed text-white px-3 py-2.5 rounded-xl text-xl font-medium"
+          href={"/"}
+        >
+          EVVENT
+        </Link>
+        <ul className="flex flex-row items-center gap-10 text-lg">
+          <li>Home</li>
+          <li>Dashboard</li>
+        </ul>
+      </div>
+      <div className="flex flex-row items-start">
+        <button></button>
+        <button></button>
+        <button></button>
+        <div className="h-14 w-14 rounded-full bg-green-700"></div>
+      </div>
+    </nav>
+  );
 }
