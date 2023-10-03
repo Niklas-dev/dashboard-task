c32c19dda7a387232ce6c9c3e4e6318794ea0c21
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 16:57:54 +0200

    add transaction page list

--- src/app/dashboard/transactions/page.tsx
+++ src/app/dashboard/transactions/page.tsx
@@ -1,4 +1,6 @@
 import DashboardRouteSwitch from "@/components/DashboardRouteSwitch";
+import TransactionListContainer from "@/components/TransactionListContainer";
+import { transactionData } from "@/mock-data/data";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,9 +9,10 @@ export const metadata: Metadata = {
 };
 export default function Page() {
   return (
-    <div className="p-2 flex flex-col gap-5">
+    <div className="p-2 flex flex-col gap-5 pb-8">
       <h1 className="text-4xl font-semibold mt-20">Dashboard</h1>
       <DashboardRouteSwitch />
+      <TransactionListContainer data={transactionData} />
     </div>
   );
 }
