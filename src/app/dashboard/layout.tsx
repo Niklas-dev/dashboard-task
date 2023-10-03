923dbda3824cd9d068e830dc1572080a265a94e2
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 19:07:32 +0200

    fix layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,15 +8,17 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <section className="flex flex-col overflow-x-hidden">
-      <div className="flex flex-row h-screen w-screen ">
-        <SideBar />
-        <main className="w-screen overflow-x-hidden">
-          <header className="w-full">
-            <NavBar />
-          </header>
-          <div className="px-4">{children}</div>
-        </main>
+    <section className="flex flex-col overflow-x-hidden h-screen w-screen overflow-y-scroll">
+      <div className="flex flex-col">
+        <div className="flex flex-row  ">
+          <SideBar />
+          <main className="w-screen overflow-x-hidden">
+            <header className="w-full">
+              <NavBar />
+            </header>
+            <div className="px-4">{children}</div>
+          </main>
+        </div>
       </div>
       <Footer />
     </section>
