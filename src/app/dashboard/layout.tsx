6fed094a7d22ce8885684572f6b63d44baed773e
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 18:57:28 +0200

    fix client server differ issue

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,25 +2,23 @@ import NavBar from "@/components/NavBar";
 import SideBar from "@/components/SideBar";
 import Footer from "@/components/Footer";
 
-export default function RootLayout({
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="flex flex-col overflow-x-hidden">
-        <div className="flex flex-row h-screen w-screen ">
-          <SideBar />
-          <main className="w-screen overflow-x-hidden">
-            <header className="w-full">
-              <NavBar />
-            </header>
-            <div className="px-4">{children}</div>
-          </main>
-        </div>
-        <Footer />
-      </body>
-    </html>
+    <section className="flex flex-col overflow-x-hidden">
+      <div className="flex flex-row h-screen w-screen ">
+        <SideBar />
+        <main className="w-screen overflow-x-hidden">
+          <header className="w-full">
+            <NavBar />
+          </header>
+          <div className="px-4">{children}</div>
+        </main>
+      </div>
+      <Footer />
+    </section>
   );
 }
