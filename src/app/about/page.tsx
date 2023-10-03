f148647a9312325e8b8e658291537906f11adede
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 16:56:26 +0200

    add placeholder main routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About",
+  description: "The Page About",
+};
+
+export default function Page() {
+  return <div>About</div>;
+}
---
+++ src/app/explore/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Explore",
+  description: "The Page Explore",
+};
+
+export default function explore() {
+  return <div>explore</div>;
+}
