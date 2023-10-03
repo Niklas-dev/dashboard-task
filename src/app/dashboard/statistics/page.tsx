f1f5c52e5921e8e0960fd70192b00d9ff6c4135e
Niklas Ladehoff <business-mail-niklas@web.de>
2023-10-03 17:25:25 +0200

    add statistics

---
+++ src/app/dashboard/statistics/page.tsx
@@ -0,0 +1,16 @@
+import DashboardReturnButton from "@/components/DashboardReturnButton";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard | Statistics",
+  description: "The Sub Page Statistics",
+};
+export default function Page() {
+  return (
+    <div className="p-2 flex flex-col gap-5 pb-8">
+      <h1 className="text-4xl font-semibold mt-20">Dashboard - Statistics</h1>
+      <DashboardReturnButton />
+    </div>
+  );
+}
